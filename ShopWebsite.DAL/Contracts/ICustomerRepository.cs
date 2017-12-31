605c152c50fff9033494cf8be23adbcbca557c68
Kha Giai Minh <trainer1234.giaiminh@gmail.com>
2017-12-31 15:46:29 +0700

    update customer repository

--- ShopWebsite.DAL/Contracts/ICustomerRepository.cs
+++ ShopWebsite.DAL/Contracts/ICustomerRepository.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using ShopWebsite.DAL.Models.CustomerModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ShopWebsite.DAL.Contracts
 {
     public interface ICustomerRepository
     {
+        Task<bool> Add(Customer newCustomer);
+        Task<bool> Edit(Customer newCustomer);
+        Task<bool> Remove(string customerId);
+        Task<Customer> Get(string customerId);
+        Task<List<Customer>> GetAll();
     }
 }
--- ShopWebsite.DAL/Implementations/CustomerRepository.cs
+++ ShopWebsite.DAL/Implementations/CustomerRepository.cs
@@ -1,8 +1,12 @@
-﻿using ShopWebsite.DAL.Context;
+﻿using Microsoft.EntityFrameworkCore;
+using ShopWebsite.DAL.Context;
 using ShopWebsite.DAL.Contracts;
+using ShopWebsite.DAL.Models.CustomerModels;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ShopWebsite.DAL.Implementations
 {
@@ -15,6 +19,80 @@ namespace ShopWebsite.DAL.Implementations
             _context = context;
         }
 
+        public async Task<bool> Add(Customer newCustomer)
+        {
+            var customerExist = await _context.Customers.Where(cust => cust.Email == newCustomer.Email).Take(1).ToListAsync();
+            if(customerExist != null && customerExist.Count > 0)
+            {
+                return false;
+            }
+            else
+            {
+                _context.Add(newCustomer);
+                _context.SaveChanges();
 
+                return true;
+            }
+        }
+
+        public async Task<bool> Edit(Customer newCustomer)
+        {
+            var customerExist = await _context.Customers.Where(cust => cust.Email == newCustomer.Email).Take(1).ToListAsync();
+            if (customerExist != null && customerExist.Count > 0)
+            {
+                var searchCustomer = customerExist[0];
+
+                searchCustomer.Address = newCustomer.Address;
+                searchCustomer.FullName = newCustomer.FullName;
+                searchCustomer.Phone = newCustomer.Phone;
+                
+                _context.Update(searchCustomer);
+                _context.SaveChanges();
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<Customer> Get(string customerId)
+        {
+            var customers = await _context.Customers.Where(cust => cust.Id == customerId).ToListAsync();
+
+            if(customers != null && customers.Count > 0)
+            {
+                var customer = customers[0];
+
+                return customer;
+            }
+            return null;
+        }
+
+        public async Task<List<Customer>> GetAll()
+        {
+            var customers = await _context.Customers.ToListAsync();
+
+            return customers;
+        }
+
+        public async Task<bool> Remove(string customerId)
+        {
+            var customerExist = await _context.Customers.Where(cust => cust.Id == customerId).Take(1).ToListAsync();
+            if (customerExist != null && customerExist.Count > 0)
+            {
+                var searchCustomer = customerExist[0];
+
+                _context.Remove(searchCustomer);
+                _context.SaveChanges();
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
