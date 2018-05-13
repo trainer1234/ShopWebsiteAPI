13229b168086e099874f9630d2f7e3dc3f42c606
Kha Giai Minh <trainer1234.giaiminh@gmail.com>
2018-05-13 23:01:15 +0700

    add new role, two new tables, new migrations

---
+++ ShopWebsite.DAL/Migrations/20180513155128_AddCustomerRatingFeedbackTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ShopWebsite.DAL.Migrations
+{
+    public partial class AddCustomerRatingFeedbackTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
