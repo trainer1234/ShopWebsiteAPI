d077c41e03238b745223a35060576d0839ac60b5
Kha Giai Minh <trainer1234.giaiminh@gmail.com>
2018-05-14 18:09:49 +0700

    update new connection string to remote server

--- ShopWebsite.DAL/Context/ShopWebsiteSqlContext.cs
+++ ShopWebsite.DAL/Context/ShopWebsiteSqlContext.cs
@@ -47,7 +47,7 @@ namespace ShopWebsite.DAL.Context
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=.;Database=ShopWebsite;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer("Data Source=163.22.17.212;Initial Catalog=ShopWebsite;Integrated Security=False;User ID=sa;Password=abcd@1234;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
